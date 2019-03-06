768495abe628a4910c00a19ff1a27172c34c2e01
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-06 02:59:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@
 
 def select_winner(passengers)
   winner = ""
-  passengers.each do |suit, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
       winner = name
   end # add the code snippet here!
